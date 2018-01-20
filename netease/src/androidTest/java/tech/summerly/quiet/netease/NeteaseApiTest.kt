8c640089179e180d0a37b6c08a7f2ee6608362ab
summer <yangbinyhbn@gmail.com>
2018-01-20 21:34:23 +0800

    test : add a test

---
+++ netease/src/androidTest/java/tech/summerly/quiet/netease/NeteaseApiTest.kt
@@ -0,0 +1,32 @@
+package tech.summerly.quiet.netease
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import tech.summerly.quiet.commonlib.utils.log
+import tech.summerly.quiet.netease.api.NeteaseCloudMusicApi
+
+/**
+ * author: summerly
+ * email: yangbinyhbn@gmail.com
+ */
+@RunWith(AndroidJUnit4::class)
+class NeteaseApiTest {
+
+    private val neteaseApi = NeteaseCloudMusicApi(InstrumentationRegistry.getTargetContext())
+
+    @Test
+    fun getMusicDetail() {
+        runBlocking {
+            val result = neteaseApi.getMusicDetail(530986445)
+            log { result }
+        }
+    }
+
+    @Test
+    fun test() {
+        log { "test" }
+    }
+}
