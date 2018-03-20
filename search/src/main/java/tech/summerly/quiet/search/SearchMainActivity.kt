eb2674f812c04ef7bedabdd8d8f6c8d523b60ba6
summerly <yangbinyhbn@gmail.com>
2018-03-20 16:03:07 +0800

    pref : 修改搜索框布局

--- search/src/main/java/tech/summerly/quiet/search/SearchMainActivity.kt
+++ search/src/main/java/tech/summerly/quiet/search/SearchMainActivity.kt
@@ -2,20 +2,14 @@ package tech.summerly.quiet.search
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.EditorInfo
+import android.support.v7.widget.SearchView
 import android.view.inputmethod.InputMethodManager
 import com.alibaba.android.arouter.facade.annotation.Route
 import kotlinx.android.synthetic.main.search_activity_main.*
 import kotlinx.coroutines.experimental.launch
-import org.jetbrains.anko.toast
 import tech.summerly.quiet.commonlib.base.BaseActivity
 import tech.summerly.quiet.commonlib.fragments.BottomControllerFragment
-import tech.summerly.quiet.commonlib.utils.gone
 import tech.summerly.quiet.commonlib.utils.log
-import tech.summerly.quiet.commonlib.utils.visible
 import tech.summerly.quiet.constraints.Search
 import tech.summerly.quiet.search.fragments.SearchDefaultFragment
 import tech.summerly.quiet.search.fragments.SearchResultsFragment
@@ -36,68 +30,38 @@ class SearchMainActivity : BaseActivity(), BottomControllerFragment.BottomContro
         initView()
     }
 
-    private val voiceClickListener = { _: View ->
-        toast("the function is not implemented")
-        Unit
-    }
-
-    private val queryClickListener = { _: View ->
-        startQuery()
-    }
-
     private fun initView() {
         buttonBack.setOnClickListener {
             onBackPressed()
         }
-        buttonIndicator.setOnClickListener(voiceClickListener)
-        buttonClear.setOnClickListener {
-            editQuery.setText("")
-        }
-        editQuery.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                val text = s?.toString()
-                if (text == null || text.isEmpty()) {
-                    buttonIndicator.setImageResource(R.drawable.search_ic_keyboard_voice_black_24dp)
-                    buttonIndicator.setOnClickListener(voiceClickListener)
-                    buttonClear.gone()
-                } else {
-                    buttonIndicator.setImageResource(R.drawable.search_ic_search_black_24dp)
-                    buttonIndicator.setOnClickListener(queryClickListener)
-                    buttonClear.visible()
-                }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                startQuery(query)
+                return true
             }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
             }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
         })
-        editQuery.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_NONE) {
-                startQuery()
-            }
-            false
-        }
+
+        searchView.isIconified = false
         supportFragmentManager.inTransaction {
             replace(R.id.layoutContainer, SearchDefaultFragment())
         }
     }
 
-    fun startQuery(text: String = editQuery.text.toString().trim()) {
+    fun startQuery(text: String = searchView.query.toString().trim()) {
         log { "查询:$text" }
         if (text.isEmpty()) {
             //do nothing
             return
         }
-        if (text != editQuery.text.toString()) {
-            editQuery.setText(text)
+        if (text != searchView.query.toString()) {
+            searchView.setQuery(text, false)
         }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(editQuery.windowToken, 0)
+        imm.hideSoftInputFromWindow(searchView.windowToken, 0)
         val fragment = SearchResultsFragment.newInstance(text)
         supportFragmentManager.inTransaction {
             replace(R.id.layoutContainer, fragment)
