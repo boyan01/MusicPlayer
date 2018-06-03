1180faad6a1070f86313d9dabb81a7b1fd8c46d9
summerly <yangbinyhbn@gmail.com>
2018-06-03 09:42:29 +0800

    add netease main menu

--- netease/src/main/java/tech/summerly/quiet/netease/fragments/NeteaseMainFragment.kt
+++ netease/src/main/java/tech/summerly/quiet/netease/fragments/NeteaseMainFragment.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import android.support.v7.widget.SimpleItemAnimator
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.alibaba.android.arouter.launcher.ARouter
 import kotlinx.android.synthetic.main.netease_fragment_main.view.*
 import tech.summerly.quiet.commonlib.base.BaseFragment
 import tech.summerly.quiet.commonlib.component.callback.BottomControllerHost
 import tech.summerly.quiet.commonlib.utils.asyncUI
+import tech.summerly.quiet.constraints.Search
+import tech.summerly.quiet.constraints.Setting
 import tech.summerly.quiet.netease.R
 import tech.summerly.quiet.netease.adapters.MainRecyclerAdapter
 import tech.summerly.quiet.netease.persistence.NeteasePreference
+import tech.summerly.quiet.netease.utils.logout
 import tech.summerly.quiet.service.netease.NeteaseCloudMusicApi
 
-class NeteaseMainFragment : BaseFragment(), BottomControllerHost {
-
+class NeteaseMainFragment : BaseFragment(), BottomControllerHost, Toolbar.OnMenuItemClickListener {
 
     private val adapter by lazy { MainRecyclerAdapter() }
 
@@ -28,8 +32,28 @@ class NeteaseMainFragment : BaseFragment(), BottomControllerHost {
         super.onViewCreated(view, savedInstanceState)
         recycler.adapter = adapter
         (recycler.itemAnimator as SimpleItemAnimator?)?.supportsChangeAnimations = false
+        toolbar.inflateMenu(R.menu.netease_menu_main)
+        toolbar.setOnMenuItemClickListener(this@NeteaseMainFragment)
         adapter.show()
     }
+
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.netease_menu_search -> {
+                ARouter.getInstance().build(Search.ACTIVITY_SEARCH_MAIN).navigation()
+            }
+            R.id.netease_menu_main_logout -> {
+                logout()
+                onStart()
+            }
+            R.id.netease_menu_main_setting -> {
+                ARouter.getInstance().build(Setting.ACTIVITY_SETTING_MAIN).navigation()
+            }
+        }
+        return true
+    }
+
 
     override fun onStart() {
         super.onStart()
