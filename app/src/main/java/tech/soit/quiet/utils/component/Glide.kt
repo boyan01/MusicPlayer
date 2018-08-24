6eb57fc4d1122312590075f789ad930ae837c925
伯言 <yangbinyhbn@gmail.com>
2018-08-24 11:36:14 +0800

    add glide

---
+++ app/src/main/java/tech/soit/quiet/utils/component/Glide.kt
@@ -0,0 +1,8 @@
+package tech.soit.quiet.utils.component
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule(glideName = "ImageLoader")
+class MyAppGlideModule : AppGlideModule()
