19333231f6ebfc43b8b94ac4d4cccb70cea7d10f
伯言 <yangbinyhbn@gmail.com>
2018-09-01 16:55:51 +0800

    remove other modules

--- commonlib/src/main/java/tech/summerly/quiet/commonlib/player/PlayerType.kt
+++
@@ -1,6 +0,0 @@
-package tech.summerly.quiet.commonlib.player
-
-enum class PlayerType {
-    FM,
-    NORMAL
-}
--- commonlib/src/main/java/tech/summerly/quiet/service/netease/result/CommonResultBean.kt
+++
@@ -1,7 +0,0 @@
-package tech.summerly.quiet.service.netease.result
-
-/**
- * Created by Summerly on 2017/10/2.
- * Desc:
- */
-class CommonResultBean(val code: Int)
