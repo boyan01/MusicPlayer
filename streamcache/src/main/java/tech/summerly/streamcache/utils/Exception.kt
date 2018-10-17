119db5de0f56b46fb88bd9bebc2367b7a52cc432
伯言 <yangbinyhbn@gmail.com>
2018-10-17 19:41:06 +0800

    remove module stream cache

--- streamcache/src/main/java/tech/summerly/streamcache/utils/Exception.kt
+++
@@ -1,7 +0,0 @@
-package tech.summerly.streamcache.utils
-
-import java.io.IOException
-
-open class StreamCacheException(msg: String) : IOException(msg)
-
-class StreamOpenException(msg: String) : StreamCacheException(msg)
